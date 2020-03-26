2910c6025140f15a86caa9f53a8ce1aba83928d4
craig beswetherick <craigbeswetherick@gmail.com>
2020-03-26 15:31:35 +0000

    initial build

---
+++ server/src/Utils/constants.js
@@ -0,0 +1,12 @@
+exports.corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200
+};
+
+exports.databaseConfig = {
+  databaseName: "myDb",
+  databaseUser: "sa",
+  databasePassword: "Password31!",
+  databaseHost: "localhost",
+  databasePort: 1401
+};
